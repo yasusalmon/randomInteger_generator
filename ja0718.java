739b9d1ae494a2a3ce7ef893a9d4ca31a65007a6
yasu <nekononikukyu0424@gmail.com>
2022-07-19 09:59:04 +0900

    chotto shuusei

--- ja0718.java
+++ ja0718.java
@@ -4,10 +4,11 @@ public class ja0718{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("hello random");
-    System.out.print("min:0 max:");
+    System.out.print("min:1 max:");
     int input = sc.nextInt();
+    if(input < 1)input = 1;
     Random rand = new Random();
-    int generate = rand.nextInt(input + 1);
+    int generate = rand.nextInt(input) + 1;
     System.out.println("");
     System.out.println("answer is " + generate);
   }
